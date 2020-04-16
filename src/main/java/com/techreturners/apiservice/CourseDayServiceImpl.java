29b0e0a129ac7dbc72195bd0eb73392e1de035a1
iggy-pigy <ilgakokorisa@gmail.com>
2020-04-16 22:35:01 +0100

    Example push

--- src/main/java/com/techreturners/apiservice/CourseDayServiceImpl.java
+++ src/main/java/com/techreturners/apiservice/CourseDayServiceImpl.java
@@ -24,7 +24,7 @@ public class CourseDayServiceImpl implements CourseDayService {
                 break;
             }
             case 4: {
-                description = "CI/CD with Jenkins";
+                description = "CI/CD with Jenkins and DevOps";
                 break;
             }
             case 5: {
